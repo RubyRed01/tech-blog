03b518fb656f1cbabfe864cc9662822659a29e9d
RubyRed01 <104714810+RubyRed01@users.noreply.github.com>
2022-09-06 02:25:37 +1000

    editing handlebars

--- Main/public/js/profile.js
+++ Main/public/js/profile.js
@@ -1,12 +1,12 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#project-name').value.trim();
-    const needed_funding = document.querySelector('#project-funding').value.trim();
-    const description = document.querySelector('#project-desc').value.trim();
+    const name = document.querySelector('#blogpost-name').value.trim();
+    const needed_funding = document.querySelector('#blogpost-funding').value.trim();
+    const description = document.querySelector('#blogpost-desc').value.trim();
   
     if (name && needed_funding && description) {
-      const response = await fetch(`/api/projects`, {
+      const response = await fetch(`/api/blogposts`, {
         method: 'POST',
         body: JSON.stringify({ name, needed_funding, description }),
         headers: {
@@ -17,7 +17,7 @@ const newFormHandler = async (event) => {
       if (response.ok) {
         document.location.replace('/profile');
       } else {
-        alert('Failed to create project');
+        alert('Failed to create blogpost');
       }
     }
   };
@@ -26,23 +26,23 @@ const newFormHandler = async (event) => {
     if (event.target.hasAttribute('data-id')) {
       const id = event.target.getAttribute('data-id');
   
-      const response = await fetch(`/api/projects/${id}`, {
+      const response = await fetch(`/api/blogposts/${id}`, {
         method: 'DELETE',
       });
   
       if (response.ok) {
         document.location.replace('/profile');
       } else {
-        alert('Failed to delete project');
+        alert('Failed to delete blogpost');
       }
     }
   };
   
   document
-    .querySelector('.new-project-form')
+    .querySelector('.new-blogpost-form')
     .addEventListener('submit', newFormHandler);
   
   document
-    .querySelector('.project-list')
+    .querySelector('.blogpost-list')
     .addEventListener('click', delButtonHandler);
   
