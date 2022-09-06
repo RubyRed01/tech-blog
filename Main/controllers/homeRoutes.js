60765d37fc3bdb9649c6eda4a22e2ee2265a50da
RubyRed01 <104714810+RubyRed01@users.noreply.github.com>
2022-09-06 21:07:22 +1000

    fixed logout issue

--- Main/controllers/homeRoutes.js
+++ Main/controllers/homeRoutes.js
@@ -27,6 +27,31 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/dashboard', async (req, res) => {
+  try {
+    // Get all BlogPosts and JOIN with user data
+    const blogPostData = await BlogPost.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const BlogPosts = blogPostData.map((BlogPost) => BlogPost.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('dashboard', { 
+      BlogPosts, 
+      logged_in: req.session.logged_in 
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
 router.get('/BlogPost/:id', async (req, res) => {
   try {
     const blogPostData = await BlogPost.findByPk(req.params.id, {
--- Main/public/js/profile.js
+++ Main/public/js/profile.js
@@ -1,14 +1,13 @@
 const newFormHandler = async (event) => {
     event.preventDefault();
   
-    const name = document.querySelector('#blogpost-name').value.trim();
-    const needed_funding = document.querySelector('#blogpost-funding').value.trim();
-    const description = document.querySelector('#blogpost-desc').value.trim();
+    const title = document.querySelector('#blogpost-title').value.trim();
+    const content = document.querySelector('#blogpost-content').value.trim();
   
-    if (name && needed_funding && description) {
+    if (title && content) {
       const response = await fetch(`/api/blogposts`, {
         method: 'POST',
-        body: JSON.stringify({ name, needed_funding, description }),
+        body: JSON.stringify({ title, content }),
         headers: {
           'Content-Type': 'application/json',
         },
